5f8d16fb8f67db803cf372bc355d0f1bf25259c4
NXTHING <nxthing.az@gmail.com>
2022-11-16 05:26:40 +0400

    a new changes in Application class

--- TestApplication.java
+++ TestApplication.java
@@ -1,6 +1,6 @@
 public class TestApplication {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("It is a new A branch");
+        System.out.println("I just made a new conflict");
     }
 }
