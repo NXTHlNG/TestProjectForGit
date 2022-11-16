30a4ccec94fe81a8efbc04a8c8c9b6bd24acb613
NXTHING <nxthing.az@gmail.com>
2022-11-16 05:15:52 +0400

    TestApplication class was changed

--- TestApplication.java
+++ TestApplication.java
@@ -1,5 +1,6 @@
 public class TestApplication {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("It is a new A branch");
     }
 }
